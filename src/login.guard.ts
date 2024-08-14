bf848eb37ddac654b18f83d43bc42ba281696fec
dongyuekai <457545648@qq.com>
2024-08-14 14:04:17 +0800

    feat: LoginGuard权限中间件控制路由访问权限

---
+++ src/login.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log('login check---');
+    // 返回false表示权限禁止
+    return false;
+  }
+}
